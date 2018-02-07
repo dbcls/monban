ec677ef36ae00162611339cd2fd6499810e92937
Yoji Shidara <dara@shidara.net>
2018-02-07 11:57:57 +0900

    Add list of types to support

--- src/validators/Literal.ts
+++ src/validators/Literal.ts
@@ -3,6 +3,18 @@ import { TriplewiseValidator } from "../TriplewiseValidator";
 import { ValidationError } from "../ValidationError";
 import { Triple } from "../Triple";
 
+/*
+xsd:anyURI
+xsd:string
+xsd:dateTime
+xsd:integer
+xsd:float
+xsd:decimal
+xsd:double
+xsd:time
+xsd:date
+xsd:int
+*/
 
 export class Literal extends TriplewiseValidator {
   validate(triple: Triple): ValidationError[] {
@@ -16,7 +28,6 @@ export class Literal extends TriplewiseValidator {
         if (!value.match(/^[+-]?[0-9]+$/)) {
           return [{message: `illegal integer value '${value}'`}];
         }
-
     }
     return [];
   }
