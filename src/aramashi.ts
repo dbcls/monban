74c4944fb8c96713ff0678796bc5a8bfe9308a4b
Yoji Shidara <dara@shidara.net>
2018-02-19 12:15:25 +0900

    Support link counting

--- src/aramashi.ts
+++ src/aramashi.ts
@@ -1,11 +1,13 @@
 import * as fs from "fs";
+import * as zlib from "zlib";
 import { Readable, Writable } from "stream";
+
 import { Triple } from "./Triple";
 
 import * as N3 from "n3";
-import * as zlib from "zlib";
-
 import { N3StreamParser } from "n3";
+import * as commander from "commander";
+import * as csvParse from "csv-parse";
 
 function tripleStream(path: string): N3StreamParser {
     const streamParser = N3.StreamParser();
@@ -25,6 +27,7 @@ class Statistics {
     objectOccurrencies: { [key: string]: number } = {};
     predicateOccurencies: { [key: string]: number } = {};
     datatypeOccurrencies: { [key: string]: number } = {};
+    linkOccurrencies: { [key: string]: number } = {};
     numTriples = 0;
     numSubjects = 0;
     numObjects = 0;
@@ -32,6 +35,7 @@ class Statistics {
     numClasses = 0;
     numPredicates = 0;
     numDatatypes = 0;
+    numLinks = 0;
 
     computeDerivations() {
         this.numSubjects = Object.keys(this.subjectOccurrencies).length;
@@ -40,16 +44,20 @@ class Statistics {
         this.numLiterals = Object.keys(this.objectOccurrencies).filter((o) => N3.Util.isLiteral(o)).length;
         this.numPredicates = Object.keys(this.predicateOccurencies).length;
         this.numDatatypes = Object.keys(this.datatypeOccurrencies).length;
+        this.numLinks = Object.keys(this.linkOccurrencies).reduce((acc, k): number => acc + this.linkOccurrencies[k], 0);
     }
 }
 
 const rdfType = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type';
+const rdfsSeeAlso = 'http://www.w3.org/2000/01/rdf-schema#seeAlso';
 
 class Consumer extends Writable {
     statictics = new Statistics();
+    uriWhitelistPattern: UriWhitelist;
 
-    constructor() {
+    constructor(uriWhitelistPattern: UriWhitelist) {
         super({ objectMode: true })
+        this.uriWhitelistPattern = uriWhitelistPattern;
     }
 
     _write(triple: Triple, encoding: string, done: () => void) {
@@ -73,15 +81,98 @@ class Consumer extends Writable {
 
         // NOTE below is too memory consuming for some datasets
         st.objectOccurrencies[o] = (st.objectOccurrencies[o] === undefined ? 0 : st.objectOccurrencies[o]) + 1;
+
+        if (p === rdfsSeeAlso) {
+            const e = this.uriWhitelistPattern.match(s)
+            if (e) {
+                const ln = e.name;
+                st.linkOccurrencies[ln] = (st.linkOccurrencies[ln] === undefined ? 0 : st.linkOccurrencies[ln]) + 1;
+            }
+        }
+
         done();
     }
 }
 
+class UriWhitelistEntry {
+    name: string;
+    regexp: RegExp;
+
+    constructor(data: any) {
+        this.name = data.name;
+        this.regexp = new RegExp(data.pattern);
+    }
+
+    match(str: string): boolean {
+        return !!str.match(this.regexp);
+    }
+}
+
+class UriWhitelist {
+    entries: UriWhitelistEntry[];
+    constructor(data: any[]) {
+        this.entries = data.map((item) => new UriWhitelistEntry(item));
+    }
+
+    match(str: string): UriWhitelistEntry | undefined {
+        return this.entries.find((e) => e.match(str));
+    }
+}
+
 class Aramashi {
+    uriWhitelist: UriWhitelist = new UriWhitelist([]);
+    commander: commander.Command = commander;
+    constructor(argv: string[]) {
+        this.commander
+            .usage('[options] <file ...>')
+            .option('--link-patterns <path>', 'path to link pattern definition')
+            .parse(argv);
+    }
+
+     loadLinkPatternsTsv(fn: string): Promise<{[key: string]: string}[]> {
+        return new Promise((resolve, reject) => {
+            const parser = new csvParse.Parser({ delimiter: "\t" });
+
+            const s = fs.createReadStream(fn);
+            const list: {[key: string]: string}[] = [];
+
+            class consumer extends Writable {
+                constructor() {
+                    super({ objectMode: true })
+                }
+
+                _write(data: any, encoding: string, done: () => void) {
+                    list.push({ name: data[0], pattern: data[1] });
+                    done();
+                }
+            }
+
+            s.pipe(parser).pipe(new consumer()).on('finish', () => {
+                resolve(list);
+            });
+        });
+    }
+
+    async run() {
+        if (this.commander.args.length == 0) {
+            this.commander.help();
+        }
+
+        if (this.commander.linkPatterns) {
+            const list = await this.loadLinkPatternsTsv(this.commander.linkPatterns);
+            this.uriWhitelist = new UriWhitelist(list);
+        }
+
+        this.commander.args.forEach(async (fn) => {
+            const r = await this.statistics(fn)
+            console.log(JSON.stringify(r, null, 2));
+        });
+    };
+
     statistics(path: string) {
         const stream = tripleStream(path);
 
-        const consumer = new Consumer();
+        const consumer = new Consumer(this.uriWhitelist);
         stream.pipe(consumer);
 
         return new Promise((resolve, reject) => {
