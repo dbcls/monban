6c3286ee33e0d30545cfc798fb9a01e553d9d809
Yoji Shidara <dara@shidara.net>
2018-03-06 15:23:01 +0900

    Don't check subjects

--- src/validators/WithOntology.ts
+++ src/validators/WithOntology.ts
@@ -10,14 +10,8 @@ export class WithOntology extends TriplewiseValidator {
     validate(triple: Triple): ValidationError[] {
         const errors = [];
         const ont = this.config.ontology;
-        if (ont.isClass(triple.subject)) {
-            errors.push({ message: `class ${triple.subject} is used as a subject` });
-        }
         if (ont.isClass(triple.predicate)) {
             errors.push({ message: `class ${triple.predicate} is used as a property` });
-        }
-        if (ont.isProprety(triple.subject)) {
-            errors.push({ message: `property ${triple.subject} is used as a subject` });
         }
         if (ont.isProprety(triple.object)) {
             errors.push({ message: `property ${triple.object} is used as an object` });
