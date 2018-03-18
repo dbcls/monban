c1cabef31247698b3b1eca9a373cb0cf4697f1bf
Yoji Shidara <dara@shidara.net>
2018-03-18 12:23:43 +0900

    Add consts

--- src/validators/ValueWithUnit.ts
+++ src/validators/ValueWithUnit.ts
@@ -1,13 +1,16 @@
 import { TriplewiseValidator } from "../triplewise-validator";
 import { Triple } from "../triple";
 
+const sio = 'http://semanticscience.org/resource/';
+const sio000216 = sio + 'SIO_000216';
+const sio000221 = sio + 'SIO_000221';
+const sio000300 = sio + 'SIO_000300';
+
+const uoRegexp = new RegExp('http://purl.obolibrary.org/obo/UO_\d+');
+
 export class ValueWithUnit extends TriplewiseValidator {
     triple(triple: Triple) {
         // FIXME WIP
         return [];
-    }
-
-    hasOboPrefix(n: string): boolean {
-        return n.startsWith('http://purl.obolibrary.org/obo/');
     }
 }
