99db5791a6fb3151d1488d47ece12b517d64b02e
Keita Urashima <ursm@ursm.jp>
2018-03-20 12:16:40 +0900

    Support various integer types

--- src/validators/Literal.ts
+++ src/validators/Literal.ts
@@ -8,27 +8,45 @@ N3.Util; // Workaround to load N3.Util
 xsd:anyURI
 xsd:string
 xsd:dateTime
-xsd:integer
 xsd:float
 xsd:decimal
 xsd:double
 xsd:time
 xsd:date
-xsd:int
 */
+
+const pattern = {
+  integer: /^[+-]?[0-9]+$/
+};
+
+const patternFor: {[key: string]: RegExp} = {
+  'http://www.w3.org/2001/XMLSchema#integer':             pattern.integer,
+  'http://www.w3.org/2001/XMLSchema#nonPositiveInteger':  pattern.integer,
+  'http://www.w3.org/2001/XMLSchema#negativeInteger':     pattern.integer,
+  'http://www.w3.org/2001/XMLSchema#long':                pattern.integer,
+  'http://www.w3.org/2001/XMLSchema#int':                 pattern.integer,
+  'http://www.w3.org/2001/XMLSchema#short':               pattern.integer,
+  'http://www.w3.org/2001/XMLSchema#byte':                pattern.integer,
+  'http://www.w3.org/2001/XMLSchema#nonNegativeInteger':  pattern.integer,
+  'http://www.w3.org/2001/XMLSchema#unsignedLong':        pattern.integer,
+  'http://www.w3.org/2001/XMLSchema#unsignedInt':         pattern.integer,
+  'http://www.w3.org/2001/XMLSchema#unsignedShort':       pattern.integer,
+  'http://www.w3.org/2001/XMLSchema#unsignedByte':        pattern.integer,
+  'http://www.w3.org/2001/XMLSchema#positiveInteger':     pattern.integer,
+};
 
 export class Literal extends TriplewiseValidator {
   triple(triple: Triple) {
-    if (!N3.Util.isLiteral(triple.object)) {
-      return;
-    }
-    const type = N3.Util.getLiteralType(triple.object);
+    if (!N3.Util.isLiteral(triple.object)) { return; }
+
+    const type  = N3.Util.getLiteralType(triple.object);
     const value = N3.Util.getLiteralValue(triple.object);
-    switch (type) {
-      case 'http://www.w3.org/2001/XMLSchema#int':
-        if (!value.match(/^[+-]?[0-9]+$/)) {
-          this.errorOnNode(value, `illegal integer value '${value}'`);
-        }
-    }
+
+    const pattern = patternFor[type];
+
+    if (!pattern)             { return; }
+    if (value.match(pattern)) { return; }
+
+    this.errorOnNode(value, `illegal value '${value}' for ${type}`);
   }
 }
