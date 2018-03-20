bfd0a39480c6c6afc2c696c889a53937f3b51d50
Keita Urashima <ursm@ursm.jp>
2018-03-20 21:50:26 +0900

    Support other literal types

--- src/validators/Literal.ts
+++ src/validators/Literal.ts
@@ -5,34 +5,36 @@ import { ErrorLiteralFormat } from "../error";
 
 N3.Util; // Workaround to load N3.Util
 
-/*
-xsd:anyURI
-xsd:string
-xsd:dateTime
-xsd:float
-xsd:decimal
-xsd:double
-xsd:time
-xsd:date
-*/
-
 type Checker = (value: string) => boolean;
+
+const pattern: { [key: string]: RegExp } = {
+  integer: /^[+-]?[0-9]+$/,
+  decimal: /^[+-]?(?:[0-9]*\.[0-9]+|[0-9]+)$/,
+  numeric: /^[+-]?(?:[0-9]+(?:\.[0-9]*)?|\.[0-9]+)(?:[eE][+\-]?[0-9]+)?$/,
+  date: /^[+-]?\d{4}-[01]\d-[0-3]\d(?:[+-][0-2]\d:[0-5]\d|Z)?$/,
+  time: /^[0-5]\d:[0-5]\d:[0-5]\d(?:\.\d+)?(?:[+-][0-2]\d:[0-5]\d|Z)?$/,
+  dateTime: /^[+-]?\d{4}-[01]\d-[0-3]\dT[0-5]\d:[0-5]\d:[0-5]\d(?:\.\d+)?(?:[+-][0-2]\d:[0-5]\d|Z)?$/,
+};
 
 function xsd(typeName: string): string {
   return `http://www.w3.org/2001/XMLSchema#${typeName}`;
 }
 
 function match(patternName: string): Checker {
-  const pat = pattern[patternName];
-
-  return value => !!value.match(pat);
+  return value => pattern[patternName].test(value);
 }
 
-const pattern: { [key: string]: RegExp } = {
-  integer: /^[+-]?[0-9]+$/,
-  decimal: /^[+-]?(?:[0-9]*\.[0-9]+|[0-9]+)$/,
-  numeric: /^[+-]?(?:[0-9]+(?:\.[0-9]*)?|\.[0-9]+)(?:[eE][+\-]?[0-9]+)?$/,
-};
+function url(): Checker {
+  return value => {
+    try {
+      new URL(value);
+    } catch (e) {
+      return false;
+    }
+
+    return true;
+  }
+}
 
 const checkerFor: { [key: string]: Checker } = {
   [xsd('integer')]: match('integer'),
@@ -51,6 +53,10 @@ const checkerFor: { [key: string]: Checker } = {
   [xsd('decimal')]: match('decimal'),
   [xsd('float')]: match('numeric'),
   [xsd('double')]: match('numeric'),
+  [xsd('date')]: match('date'),
+  [xsd('time')]: match('time'),
+  [xsd('dateTime')]: match('dateTime'),
+  [xsd('anyURI')]: url(),
 };
 
 export class Literal extends TriplewiseValidator {
--- tests/validator.test.ts
+++ tests/validator.test.ts
@@ -1,16 +1,10 @@
 import { Validator } from '../src/validator';
 import { MonbanConfig } from '../src/monban-config';
 
-test('good-integer', async () => {
-  const v = new Validator('tests/fixtures/good-integer.nt', new MonbanConfig());
-  const r = await v.validate();
-
-  expect(r.errors.size).toEqual(0);
-});
-
-test('bad-integer', async () => {
-  const v = new Validator('tests/fixtures/bad-integer.nt', new MonbanConfig());
+test('validate literal', async () => {
+  const v = new Validator('tests/fixtures/literal.nt', new MonbanConfig());
   const r = await v.validate();
 
   expect(r.errors.size).toEqual(1);
+  expect(Array.from(r.errors.values())[0].size).toEqual(7);
 });
