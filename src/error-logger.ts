dbd2ee64ae16013c626b8f506dda5e3684d604f9
Yoji Shidara <dara@shidara.net>
2018-03-20 16:43:25 +0900

    Report errors grouped by types

--- src/error-logger.ts
+++ src/error-logger.ts
@@ -2,9 +2,26 @@ import { Triple } from "./triple";
 import { Error } from "./error";
 
 export class ErrorLogger {
-    errors: Error[] = [];
+    errorsGroupByTypes: Map<String, Set<Error>> = new Map<String, Set<Error>>();
 
     add(error: Error) {
-        this.errors.push(error);
+        const name = error.constructor.name;
+        let errors = this.errorsGroupByTypes.get(name);
+        if (!errors) {
+            errors = new Set<Error>();
+            this.errorsGroupByTypes.set(name, errors);
+        }
+        errors.add(error);
+    }
+
+    errors(): any[] {
+        const errors: any[] = [];
+        this.errorsGroupByTypes.forEach((errs, type) => {
+            errors.push({
+                type: type,
+                errors: Array.from(errs),
+            });
+        });
+        return errors;
     }
 }
